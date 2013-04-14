b0aae0fa6156883618b6536dd0117f8634594d4f
Kazuhiro Eguchi <hakuraipod@gmail.com>
2013-04-14 16:02:21 +0900

    bugfix for Utils

--- knockout/Main.hx
+++ knockout/Main.hx
@@ -5,9 +5,14 @@ class Main {
     public static function main() {
         var observable = Knockout.observable("hello");
         var value = observable.value();
-        
-        Observable.fn.newMethod = function(){
+
+        Observable.fn.newMethod = function() {
             return 100;
         };
+
+        var array1 = new Array<Int>();
+        var array2 = new Array<Int>();
+
+        Utils.arrayPushAll(array1, array2);
     }
 }
--- knockout/Utils.hx
+++ knockout/Utils.hx
@@ -4,30 +4,30 @@ import knockout.Subscribable;
 extern
 class Utils {
 
-    public static function arrayForEach(array:Array<T>, action:T -> Void):Void;
+    public static function arrayForEach<T>(array:Array<T>, action:T -> Void):Void;
 
-    public static function arrayFirst(array:Array<T>, predicate:T -> Bool, ?predicateOwner:Dynamic):Null<T>;
+    public static function arrayFirst<T>(array:Array<T>, predicate:T -> Bool, ?predicateOwner:Dynamic):Null<T>;
 
-    public static function arrayFilter(array:Array<T>, predicate:T -> Bool):Array<T>;
+    public static function arrayFilter<T>(array:Array<T>, predicate:T -> Bool):Array<T>;
 
-    public static function arrayGetDistinctValues(array:Array<T>):Array<T>;
+    public static function arrayGetDistinctValues<T>(array:Array<T>):Array<T>;
 
-    public static function arrayIndexOf(array:Array<T>, item:T):Int;
+    public static function arrayIndexOf<T>(array:Array<T>, item:T):Int;
 
-    public static function arrayMap(array:Array<T1>, mapping:T1 -> T2):Array<T2>;
+    public static function arrayMap<T1,T2>(array:Array<T1>, mapping:T1 -> T2):Array<T2>;
 
-    public static function arrayPushAll(array:Array<T>, valuesToPush:Array<T>):Array<T>;
+    public static function arrayPushAll<T>(array:Array<T>, valuesToPush:Array<T>):Array<T>;
 
-    public static function arrayRemoveItem(array:Array<T>, itemToRemove:T):Void;
+    public static function arrayRemoveItem<T>(array:Array<T>, itemToRemove:T):Void;
 
     public static function extend(target:Dynamic, source:Dynamic):Void;
 
-    @:overload(function(value:T):T{})
-    public static function peekObservable(value:Subscribable<T>):T;
+    @:overload(function <T>(value:T):T{})
+    public static function peekObservable<T>(value:Subscribable<T>):T;
 
     @:overload(function(min:Int, max:Int):Array<Int>{})
     public static function range(min:Subscribable<Int>, max:Subscribable<Int>):Array<Int>;
 
-    @:overload(function(value:T):T{})
-    public static function unwrapObservable(value:Subscribable<T>):T;
+    @:overload(function <T>(value:T):T{})
+    public static function unwrapObservable<T>(value:Subscribable<T>):T;
 }
