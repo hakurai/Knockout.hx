506bd9dfcb51ff1b07204370c0920e2857f45b0b
Kazuhiro Eguchi <hakuraipod@gmail.com>
2013-08-05 02:49:53 +0900

    add getter/setter to DependentObservable

--- knockout/DependentObservable.hx
+++ knockout/DependentObservable.hx
@@ -6,6 +6,22 @@ class DependentObservable<T> extends Subscribable<T>{
 
     public static var fn:Dynamic;
 
+    inline private function setter():T -> Void untyped {
+        return this;
+    }
+
+    inline private function getter():Void -> T untyped {
+        return this;
+    }
+
+    inline public function set(newValue:T):Void {
+        return setter()(newValue);
+    }
+
+    inline public function get():Null<T> {
+        return getter()();
+    }
+
     public function peek():Null<T>;
 
     public function isActive():Bool;
